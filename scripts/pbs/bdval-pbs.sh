0a1ce773d42ae372dcb1cd99760a5f7602a10f5c
Marko Srdanovic <marko.srdanovic@gmail.com>
2009-10-29 22:28:39 +0000

    Create the script that will actually run bdval on a cluster node with the specified properties.

---
+++ scripts/pbs/bdval-pbs.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+. /etc/profile
+
+# Absolute path to this script.
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in.
+SCRIPT_DIR=`dirname $SCRIPT`
+
+# Run bdval job
+cd $SCRIPT_DIR/data
+ant -Dsave-data-tag=@TAG@ -Dtag-description="@TAG-DESCRIPTION@" -f @PROJECT@.xml @TARGET@
+
+# Copy results back to the master node when we are done
+JOB_RESULTS_DIR=@JOB_DIR@-results/${HOSTNAME}
+ssh master01 "/bin/mkdir -p ${JOB_RESULTS_DIR}"
+scp -r *.zip logs @master01:${JOB_RESULTS_DIR}
