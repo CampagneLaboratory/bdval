ef1f5a6ffa02e5956477ff30e2e1b4844523715b
Marko Srdanovic <marko.srdanovic@gmail.com>
2009-10-29 22:52:17 +0000

    Create the pbs submission scipt. Some issues with job names etc. still exist but at this point I think all the pieces are in place.

--- scripts/pbs/bdval-pbs.sh
+++ scripts/pbs/bdval-pbs.sh
@@ -11,6 +11,6 @@ cd $SCRIPT_DIR/data
 ant -Dsave-data-tag=@TAG@ -Dtag-description="@TAG-DESCRIPTION@" -f @PROJECT@.xml @TARGET@
 
 # Copy results back to the master node when we are done
-JOB_RESULTS_DIR=@JOB_DIR@-results/${HOSTNAME}
+JOB_RESULTS_DIR=@JOB-DIR@-results/${HOSTNAME}
 ssh master01 "/bin/mkdir -p ${JOB_RESULTS_DIR}"
 scp -r *.zip logs @master01:${JOB_RESULTS_DIR}
