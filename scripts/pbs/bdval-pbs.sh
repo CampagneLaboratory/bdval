a7d05ae0b7e151f74b0efbb1e147997984ef3458
Marko Srdanovic <marko.srdanovic@gmail.com>
2009-11-02 20:29:26 +0000

    Handle the case where there is only a single condition - these cannot be submitted as an array job.

--- scripts/pbs/bdval-pbs.sh
+++ scripts/pbs/bdval-pbs.sh
@@ -9,7 +9,14 @@ SCRIPT_DIR=`dirname $SCRIPT`
 # Run bdval job
 cd $SCRIPT_DIR/data
 
-ant -propertyfile=@PROJECT@-$PBS_ARRAY_INDEX.properties -Dsave-data-tag=@TAG@ -Dtag-description="@TAG-DESCRIPTION@" -f @PROJECT@.xml @TARGET@
+# see if the job is running as an array job submission
+if [ -z "$PBS_ARRAY_INDEX" ]; then
+    PBS_ANT_PROPERTY_FILE=@PROJECT@-1.properties
+else
+    PBS_ANT_PROPERTY_FILE=@PROJECT@-$PBS_ARRAY_INDEX.properties
+fi
+
+ant -propertyfile=$PBS_ANT_PROPERTY_FILE -Dsave-data-tag=@TAG@ -Dtag-description="@TAG-DESCRIPTION@" -f @PROJECT@.xml @TARGET@
 
 # Copy results back to the master node when we are done
 JOB_RESULTS_DIR=@JOB-DIR@-results/${HOSTNAME}
