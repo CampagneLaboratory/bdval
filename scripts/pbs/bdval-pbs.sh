9ee3579fefea60160ed4cb7f577d1199d107cc56
Marko Srdanovic <marko.srdanovic@gmail.com>
2009-11-02 16:13:01 +0000

    Allow specifying a different head node for the pbs cluster.  The head node for the "wulves" is "master01" but for the rascals it is "chico"

--- scripts/pbs/bdval-pbs.sh
+++ scripts/pbs/bdval-pbs.sh
@@ -8,9 +8,10 @@ SCRIPT_DIR=`dirname $SCRIPT`
 
 # Run bdval job
 cd $SCRIPT_DIR/data
+# TODO - send various property files here
 ant -Dsave-data-tag=@TAG@ -Dtag-description="@TAG-DESCRIPTION@" -f @PROJECT@.xml @TARGET@
 
 # Copy results back to the master node when we are done
 JOB_RESULTS_DIR=@JOB-DIR@-results/${HOSTNAME}
-ssh master01 "/bin/mkdir -p ${JOB_RESULTS_DIR}"
-scp -r *.zip logs @master01:${JOB_RESULTS_DIR}
+ssh @MASTER-NODE@ "/bin/mkdir -p ${JOB_RESULTS_DIR}"
+scp -r *.zip logs @@MASTER-NODE@:${JOB_RESULTS_DIR}
