0c143b43bb802eb50cc58f3482cfd7a16c36ef9a
Marko Srdanovic <marko.srdanovic@gmail.com>
2009-11-03 20:25:02 +0000

    Since our pbs setup doesn't kill everything for us - we need to shut down the Rserve processes running.  This is ok (for now) since we take over the entire node.  At some point starting/stopping Rserve should be an option so that we can use a "fixed" set of Rserve instances.

--- scripts/pbs/stop-rserve.sh
+++ scripts/pbs/stop-rserve.sh
@@ -2,8 +2,14 @@
 
 . /etc/profile
 
-PORT=${1:-"6311"}
+# Absolute path to this script.
+SCRIPT=$(readlink -f $0)
+# Absolute path this script is in.
+SCRIPT_DIR=`dirname $SCRIPT`
 
+PORT=${1:-"6311"}
 echo "Stopping Rserve process on port $PORT"
+
+# Assume bdval resides in the same directory as this script
+cd $SCRIPT_DIR
 java -cp bdval.jar edu.cornell.med.icb.R.RUtils --port $PORT --shutdown
-exec R CMD ${RSERVE_LIB} --RS-port $PORT --no-save
