aaceab9ea58949c4de4cb84d958a8c5fa4c4bf26
Marko Srdanovic <marko.srdanovic@gmail.com>
2009-11-05 18:31:33 +0000

    Cleanup the pbs script and naming conventions a bit Go back to the original separate script.  With job arrays we no longer need to use pbsdsh so we don't need to worry about missing environment variables Just use the default log4j  configuration (info level) - if you need debug you need to change it "by hand"

--- scripts/pbs/bdval-pbs.sh
+++ scripts/pbs/bdval-pbs.sh
@@ -1,19 +1,14 @@
 #!/bin/sh
 . /etc/profile
 
-# Absolute path to this script.
-SCRIPT=$(readlink -f $0)
-# Absolute path this script is in.
-SCRIPT_DIR=`dirname $SCRIPT`
-
 # Run bdval job
-cd $SCRIPT_DIR/data
+cd $TMPDIR/@JOB-NAME@/data
 
 # see if the job is running as an array job submission
 if [ -z "$PBS_ARRAY_INDEX" ]; then
-    PBS_ANT_PROPERTY_FILE=@PROJECT@-1.properties
+    PBS_ANT_PROPERTY_FILE=../@JOB-NAME@-1.properties
 else
-    PBS_ANT_PROPERTY_FILE=@PROJECT@-$PBS_ARRAY_INDEX.properties
+    PBS_ANT_PROPERTY_FILE=../@JOB-NAME@-$PBS_ARRAY_INDEX.properties
 fi
 
 ant -propertyfile $PBS_ANT_PROPERTY_FILE -Dsave-data-tag=@TAG@ -Dtag-description="@TAG-DESCRIPTION@" -f @PROJECT@.xml @TARGET@
