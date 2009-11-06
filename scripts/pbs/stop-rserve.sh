f1249e2fad494da90aa3d8fef18a97a670973f9e
Marko Srdanovic <marko.srdanovic@gmail.com>
2009-11-06 22:15:31 +0000

    Since PBS doesn't shut everything for us, we need to BUT the qsub script may not be running from where everything else is.  We know where the jar files are (since we put them there) so use that rather than assuming they are side by side with the script.

--- scripts/pbs/stop-rserve.sh
+++ scripts/pbs/stop-rserve.sh
@@ -2,14 +2,9 @@
 
 . /etc/profile
 
-# Absolute path to this script.
-SCRIPT=$(readlink -f $0)
-# Absolute path this script is in.
-SCRIPT_DIR=`dirname $SCRIPT`
-
 PORT=${1:-"6311"}
 echo "Stopping Rserve process on port $PORT"
 
 # Assume bdval resides in the same directory as this script
-cd $SCRIPT_DIR
+cd $TMPDIR/@JOB-NAME@
 java -cp bdval.jar edu.cornell.med.icb.R.RUtils --port $PORT --shutdown
