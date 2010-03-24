7626f23eda867dd69bbc1bcc5125092931bb2a27
Marko Srdanovic <marko.srdanovic@gmail.com>
2010-03-24 19:29:55 +0000

    Quote the tag name in case it has spaces

--- scripts/sge/bdval-sge.sh
+++ scripts/sge/bdval-sge.sh
@@ -6,7 +6,7 @@
 cd $TMPDIR/@JOB-NAME@/data
 
 SGE_ANT_PROPERTY_FILE=../@JOB-NAME@-${SGE_TASK_ID}.properties
-ant -propertyfile $SGE_ANT_PROPERTY_FILE -Dsave-data-tag=@TAG@ -Dtag-description="@TAG-DESCRIPTION@" -f @PROJECT@.xml @TARGET@
+ant -propertyfile $SGE_ANT_PROPERTY_FILE -Dsave-data-tag="@TAG@" -Dtag-description="@TAG-DESCRIPTION@" -f @PROJECT@.xml @TARGET@
 
 # Copy results back to the master node when we are done
 JOB_RESULTS_DIR=@JOB-DIR@-results
